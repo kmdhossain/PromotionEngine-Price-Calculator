2e029ad75f6f2957bf615e44b716b33e91b9ccd4
K Hossain <tahs.sumon@gmail.com>
2021-12-10 00:27:59 +0000

    Added Name property for Scenario domain.

--- PromotionEngine.Domains/Scenario.cs
+++ PromotionEngine.Domains/Scenario.cs
@@ -3,6 +3,7 @@ namespace PromotionEngine.Domains
 {
     public class Scenario
     {
+        public string ScenarioName { get; set; }
         public Product Product {get;set;}
         public int Quantity { get; set; }
     }
