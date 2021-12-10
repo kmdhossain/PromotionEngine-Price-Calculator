01f5ebb52083fa5280b236caba4abd0b9526cace
K Hossain <tahs.sumon@gmail.com>
2021-12-10 00:26:47 +0000

    Added data service (in memory) for product.

---
+++ PromotionEngine.DataServices/ProductDataService.cs
@@ -0,0 +1,19 @@
+﻿using PromotionEngine.Domains;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Products
+{
+    public class ProductDataService
+    {
+        public IList<Product> GetAllProducts()
+        {
+            return new List<Product>
+            {
+                new Product { SKU = "A", UnitPrice = 50 },
+                new Product { SKU = "B", UnitPrice = 30 },
+                new Product { SKU = "C", UnitPrice = 20 },
+                new Product { SKU = "D", UnitPrice = 15 }
+            };
+        }
+    }
+}
