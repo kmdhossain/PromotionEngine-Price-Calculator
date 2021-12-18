fa0e595f92e406024a8e318c310ac82434479173
K Hossain <tahs.sumon@gmail.com>
2021-12-18 22:20:23 +0000

    improved code to removed duplicate code

--- PromotionEngine.Core/ScenarioService.cs
+++ PromotionEngine.Core/ScenarioService.cs
@@ -22,74 +22,63 @@ namespace PromotionEngine.Core
 
                 if (scenarioItem.IsCalculatedInTotal)
                     continue;
-                
+
                 var promotion = PromotionDataService.GetProductPromotion(scenarioItem.Product);
                 if (promotion == null)
                     totalCalculatedPrice += scenarioItem.Quantity * scenarioItem.Product.UnitPrice;
                 else
                 {
-                    bool isSinglePromotion = promotion.ProductAndQuantity.Count == 1;
-                    if (isSinglePromotion)
-                    {
-                        var promotionOccurance = scenarioItem.Quantity / promotion.ProductAndQuantity[scenarioItem.Product.SKU];
-                        totalCalculatedPrice += promotionOccurance * promotion.PromotionPrice;
+                    //check if all promotion items exists in scenario
+                    bool allPromotionItemsInScenario = true;
 
-                        var remainingQuantityWithOutPromotion = scenarioItem.Quantity % promotion.ProductAndQuantity[scenarioItem.Product.SKU];
-                        totalCalculatedPrice += remainingQuantityWithOutPromotion * scenarioItem.Product.UnitPrice;
-                    }
+                    foreach (var promotionSubItem in promotion.ProductAndQuantity)
+                        if (!Scenario.ScenarioItems.ContainsKey(promotionSubItem.Key))
+                        {
+                            allPromotionItemsInScenario = false;
+                            break;
+                        }
+
+                    if (!allPromotionItemsInScenario)//calculate the current item without any promotion
+                        totalCalculatedPrice += scenarioItem.Quantity * scenarioItem.Product.UnitPrice;
                     else
                     {
-                        //check if all promotion items exists in scenario
-                        bool allPromotionItemsInScenario = true;
-
+                        //find the minimum promotion occurance from scenario
+                        var promotionOccuranceInProductionCombination = int.MaxValue;
                         foreach (var promotionSubItem in promotion.ProductAndQuantity)
-                            if (!Scenario.ScenarioItems.ContainsKey(promotionSubItem.Key))
-                            {
-                                allPromotionItemsInScenario = false;
-                                break;
-                            }
-
-                        if(!allPromotionItemsInScenario)//calculate the current item without any promotion
-                            totalCalculatedPrice += scenarioItem.Quantity * scenarioItem.Product.UnitPrice;
-                        else
                         {
-                            //find the minimum promotion occurance from scenario
-                            var promotionOccuranceInProductionCombination = int.MaxValue;
-                            foreach (var promotionSubItem in promotion.ProductAndQuantity)
-                            {
-                                var scenarioSubItem = Scenario.ScenarioItems[promotionSubItem.Key];
-                                var subItemPromotionOccurance = scenarioSubItem.Quantity / promotionSubItem.Value;
-                                if (subItemPromotionOccurance < promotionOccuranceInProductionCombination)
-                                    promotionOccuranceInProductionCombination = subItemPromotionOccurance;
-                            }
+                            var scenarioSubItem = Scenario.ScenarioItems[promotionSubItem.Key];
+                            var subItemPromotionOccurance = scenarioSubItem.Quantity / promotionSubItem.Value;
+                            if (subItemPromotionOccurance < promotionOccuranceInProductionCombination)
+                                promotionOccuranceInProductionCombination = subItemPromotionOccurance;
+                        }
 
-                            if (promotionOccuranceInProductionCombination == int.MaxValue)
-                                promotionOccuranceInProductionCombination = 0;
+                        if (promotionOccuranceInProductionCombination == int.MaxValue)
+                            promotionOccuranceInProductionCombination = 0;
 
-                            //calculate total for number of promotions to be applied
-                            totalCalculatedPrice += promotionOccuranceInProductionCombination * promotion.PromotionPrice;
+                        //calculate total for number of promotions to be applied
+                        totalCalculatedPrice += promotionOccuranceInProductionCombination * promotion.PromotionPrice;
 
-                            //calculate total for items for which promotion can't be applied
-                            foreach (var promotionSubItem in promotion.ProductAndQuantity)
-                            {
-                                var scenarioSubItem = Scenario.ScenarioItems[promotionSubItem.Key];
-                                scenarioSubItem.IsCalculatedInTotal = true;
+                        //calculate total for items for which promotion can't be applied
+                        foreach (var promotionSubItem in promotion.ProductAndQuantity)
+                        {
+                            var scenarioSubItem = Scenario.ScenarioItems[promotionSubItem.Key];
+                            scenarioSubItem.IsCalculatedInTotal = true;
 
-                                //find remaining items that included in promotion quantity for the item
-                                var remainingQuantityWithOutPromotion = scenarioSubItem.Quantity % promotionSubItem.Value;
-                                
-                                //find remaining items that couldn't be included due to promotion combinations
-                                remainingQuantityWithOutPromotion += scenarioSubItem.Quantity - (promotionOccuranceInProductionCombination * promotionSubItem.Value) - remainingQuantityWithOutPromotion;
+                            //find remaining items that included in promotion quantity for the item
+                            var remainingQuantityWithOutPromotion = scenarioSubItem.Quantity % promotionSubItem.Value;
 
-                                totalCalculatedPrice += remainingQuantityWithOutPromotion * scenarioSubItem.Product.UnitPrice;
-                            }
+                            //find remaining items that couldn't be included due to promotion combinations
+                            remainingQuantityWithOutPromotion += scenarioSubItem.Quantity - (promotionOccuranceInProductionCombination * promotionSubItem.Value) - remainingQuantityWithOutPromotion;
 
-                       }
+                            totalCalculatedPrice += remainingQuantityWithOutPromotion * scenarioSubItem.Product.UnitPrice;
+                        }
 
                     }
 
                 }
+
             }
+
 
             return totalCalculatedPrice;
         }
